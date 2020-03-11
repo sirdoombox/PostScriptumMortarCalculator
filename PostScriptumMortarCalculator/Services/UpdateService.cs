5508f33d6caac5ead1d41c7301a15e524ad21a7d
Doombox <Aaron.j.king.95@gmail.com>
2020-03-11 12:19:27 +0000

    Added version checking and an update button, this will serve as a test release

---
+++ PostScriptumMortarCalculator/Services/UpdateService.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace PostScriptumMortarCalculator.Services
+{
+    public class UpdateService
+    {
+        private const string c_OWNER_NAME = "sirdoombox";
+        private const string c_REPO_NAME = "PostScriptumMortarCalculator";
+        private const string c_RELEASE_PATH = "https://github.com/sirdoombox/PostScriptumMortarCalculator/releases/latest";
+        
+        private readonly GitHubClient m_client;
+        
+        public UpdateService()
+        {
+            m_client = new GitHubClient(new ProductHeaderValue("PostScriptumMortarCalculator"));
+        }
+        
+        public async Task<UpdateInfo> CheckForUpdate()
+        {
+#if (!DEBUG)
+            var release = await m_client.Repository.Release.GetLatest(c_OWNER_NAME, c_REPO_NAME);
+            var releaseVersion = decimal.Parse(release.TagName);
+            var currentVersionString = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var currentVersion = decimal.Parse(currentVersionString);
+            return new UpdateInfo(currentVersion, releaseVersion, c_RELEASE_PATH);
+#endif
+            return new UpdateInfo(1.0m,1.1m,c_RELEASE_PATH);
+        }
+
+        public struct UpdateInfo
+        {
+            public decimal CurrentVersion { get; }
+            public decimal NewVersion { get; }
+            public string ReleasePath { get; }
+
+            public bool IsUpdateAvailable => NewVersion > CurrentVersion;
+
+            public UpdateInfo(decimal currentVersion, decimal newVersion, string releasePath)
+            {
+                CurrentVersion = currentVersion;
+                NewVersion = newVersion;
+                ReleasePath = releasePath;
+            }
+        }
+    }
+}
--- PostScriptumMortarCalculator/ViewModels/PsmcRootViewModel.cs
+++ PostScriptumMortarCalculator/ViewModels/PsmcRootViewModel.cs
@@ -1,5 +1,7 @@
 using System.Windows;
+using System.Windows.Controls;
 using MahApps.Metro.Controls;
+using PostScriptumMortarCalculator.Services;
 using Stylet;
 
 namespace PostScriptumMortarCalculator.ViewModels
@@ -9,17 +11,40 @@ namespace PostScriptumMortarCalculator.ViewModels
         public MenuFlyoutViewModel MenuFlyoutViewModel { get; set; }
         public MapViewModel MapViewModel { get; set; }
         public CalculatorViewModel CalculatorViewModel { get; set; }
+        
+        public bool IsUpdateAvailable { get; private set; }
+
+        public string UpdateTooltip { get; private set; }
+
+        private readonly UpdateService m_updateService;
+        
+        private string m_updatePath;
 
         public PsmcRootViewModel(CalculatorViewModel calcViewModel,
+            UpdateService updateService,
             MapViewModel mapViewModel,
             MenuFlyoutViewModel menuFlyoutViewModel)
         {
             MapViewModel = mapViewModel;
             ActivateItem(MapViewModel);
             CalculatorViewModel = calcViewModel;
+            m_updateService = updateService;
             ActivateItem(CalculatorViewModel);
             MenuFlyoutViewModel = menuFlyoutViewModel;
             ActivateItem(MenuFlyoutViewModel);
+        }
+
+        protected override async void OnViewLoaded()
+        {
+            var result = await m_updateService.CheckForUpdate();
+            IsUpdateAvailable = result.IsUpdateAvailable;
+            m_updatePath = result.ReleasePath;
+            UpdateTooltip = $"Current: {result.CurrentVersion} | New: {result.NewVersion}";
+        }
+
+        public void UpdateAvailableClicked()
+        {
+            System.Diagnostics.Process.Start(m_updatePath);
         }
 
         public void OpenMenuFlyout()
